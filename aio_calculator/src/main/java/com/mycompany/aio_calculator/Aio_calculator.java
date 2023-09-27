02ef72510b33265af0f28cdc63dbaba23809bc15
leafanine <@REDSKULL-062>
2023-09-27 15:14:30 +0530

    initial commit

---
+++ aio_calculator/src/main/java/com/mycompany/aio_calculator/Aio_calculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aio_calculator;
+
+/**
+ *
+ * @author subha
+ */
+public class Aio_calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
